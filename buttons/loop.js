9134bf463bafbc3c938c9571019cb5228590021d
weibb123 <weining563@gmail.com>
2025-01-05 16:59:00 -0500

    display message when loop

--- buttons/loop.js
+++ buttons/loop.js
@@ -2,11 +2,22 @@ const { QueueRepeatMode } = require('discord-player');
 const { Translate } = require('../process_tools');
 
 module.exports = async ({ inter, queue }) => {
+
+    const userMention = `<@${inter.member.id}>`;
+
     const methods = ['disabled', 'track', 'queue'];
+
     if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`No music currently playing... try again ? <❌>`) });
 
-    if (queue.repeatMode === 2) queue.setRepeatMode(QueueRepeatMode.OFF)
-    else queue.setRepeatMode(queue.repeatMode + 1)
+    if (queue.repeatMode === 2) {
+        queue.setRepeatMode(QueueRepeatMode.OFF);
+    } else {
+        queue.setRepeatMode(queue.repeatMode + 1);
+    }
 
-    return inter.editReply({ content: await Translate(`Loop made has been set to <**${methods[queue.repeatMode]}**>.<✅>`) });
+    const trackName = queue.currentTrack.title;
+
+    return inter.editReply({
+        content: await Translate(`${userMention}, 你想唱死我莫 | <**${methods[queue.repeatMode]}**> <${trackName}> loop<✅>`) 
+    });
 }
